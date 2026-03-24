822443b679b4b81d47c5fb7365082e47f7d149c9
Szymon Szot <s33692@pjwstk.edu.pl>
2026-03-24 14:28:07 +0100

    basic abstract User implementation

--- APBD_cw3/User.cs
+++ APBD_cw3/User.cs
@@ -2,4 +2,19 @@
 
 public abstract class  User
 {
+    private static int _idGen = 0;
+    
+    public int Id { get; private set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int max { get; protected set; }
+
+    protected  User(string fn, string ln) 
+    {
+        Id = _idGen++; FirstName = fn; LastName = ln;
+    }
+
+    protected User()
+    {
+    }
 }

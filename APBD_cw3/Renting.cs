5f4907a7712f14706d277efd1a24e07f586a215d
Szymon Szot <s33692@pjwstk.edu.pl>
2026-03-24 22:37:20 +0100

    Renting initial implementation

--- APBD_cw3/Renting.cs
+++ APBD_cw3/Renting.cs
@@ -2,4 +2,23 @@
 
 public class Renting
 {
+    public Item RentedItem { get; private set; }
+    public User RentingUser { get; private set; }
+    public DateTime RentDate { get; private set; }
+    public DateTime ReturnDate { get; set; }
+
+    public Renting(Item rentedItem, User rentingUser, DateTime rentDate, DateTime returnDate)
+    {
+        RentedItem = rentedItem;
+        RentingUser = rentingUser;
+        RentDate = rentDate;
+        ReturnDate = returnDate;
+    }
+
+    public double Overdue()
+    {
+        if (ReturnDate.CompareTo(DateTime.Now) < 0)
+            return ReturnDate.Subtract(DateTime.Now).TotalDays;
+        return 0.0;
+    }
 }

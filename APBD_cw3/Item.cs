ceb8f25df70184a81bb30f9987caf53a236af6b3
Szymon Szot <s33692@pjwstk.edu.pl>
2026-03-24 14:42:45 +0100

    Basic Item implementation

--- APBD_cw3/Item.cs
+++ APBD_cw3/Item.cs
@@ -1,4 +1,18 @@
 ﻿namespace APBD_cw3;
 public abstract class Item
 {
+    private static int _idGen = 0;
+    
+    public int Id { get; private set; }
+    public string Name { get; set; }
+    public bool IsAvailable { get; set; } = true;
+
+    protected Item(string name)
+    {
+        Id = _idGen++; Name = name;
+    }
+    
+    public Item()
+    {
+    }
 }

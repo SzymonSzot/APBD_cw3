96d26c36035f6b8860a4d4dbb61ba771993c35ab
Szymon Szot <s33692@pjwstk.edu.pl>
2026-03-24 14:51:37 +0100

    Laptop implementation

--- APBD_cw3/Laptop.cs
+++ APBD_cw3/Laptop.cs
@@ -2,4 +2,11 @@
 
 public class Laptop : Item
 {
+    public string Processor { get; set; }
+    public int RamGb { get; set; }
+    public Laptop(string name, string cpu, int ram) : base(name) 
+    {
+        Processor = cpu;
+        RamGb = ram;
+    }
 }

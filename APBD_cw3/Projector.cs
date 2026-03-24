9dc71dc480cd056f3d05d353b3a2a1a95333de85
Szymon Szot <s33692@pjwstk.edu.pl>
2026-03-24 14:50:27 +0100

    Projector implementation

--- APBD_cw3/Projector.cs
+++ APBD_cw3/Projector.cs
@@ -2,4 +2,12 @@
 
 public class Projector : Item
 {
+    public int BrightnessLumens { get; set; }
+    public string InputInterface { get; set; }
+
+    public Projector(string name, int lumens, string inputInterface) : base(name)
+    {
+        BrightnessLumens = lumens;
+        InputInterface = inputInterface;
+    }
 }

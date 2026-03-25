dd3010d9853a75279f89bcdf10718bcc25cc1134
Szymon Szot <s33692@pjwstk.edu.pl>
2026-03-25 21:58:37 +0100

    Renting realReturn add

--- APBD_cw3/Renting.cs
+++ APBD_cw3/Renting.cs
@@ -9,6 +9,8 @@ public class Renting
     public User RentingUser { get; private set; }
     public DateTime RentDate { get; private set; }
     public DateTime ReturnDate { get; set; }
+    
+    public DateTime RealReturnDate { get; set;}
 
     public Renting(Item rentedItem, User rentingUser, DateTime rentDate, DateTime returnDate)
     {
@@ -23,6 +25,17 @@ public class Renting
     {
         if (ReturnDate.CompareTo(DateTime.Now) < 0)
             return ReturnDate.Subtract(DateTime.Now).TotalDays;
+        return 0.0;
+    }
+
+    public double Penalty()
+    {
+        Double penaltyDayRate = 20.0;
+        
+        if (RealReturnDate != null)
+        {
+            return Overdue() * penaltyDayRate;
+        }
         return 0.0;
     }
 }

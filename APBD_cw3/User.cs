98e56dcc0e7e2c6cfa3431e197b879c5d162a6a9
Szymon Szot <s33692@pjwstk.edu.pl>
2026-03-25 19:49:35 +0100

    Users per instatance rent counter

--- APBD_cw3/User.cs
+++ APBD_cw3/User.cs
@@ -5,12 +5,14 @@ public abstract class  User
     private static int _idGen = 0;
     
     public int Id { get; private set; }
+    
+    public int RentedDevices { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public abstract int Max { get;}
 
     protected  User(string fn, string ln) 
     {
-        Id = _idGen++; FirstName = fn; LastName = ln;
+        Id = _idGen++; FirstName = fn; LastName = ln; RentedDevices = 0;
     }
 }

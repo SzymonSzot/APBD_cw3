2dad0decf196c6a3f968ff90112b83d8d8c767f2
Szymon Szot <s33692@pjwstk.edu.pl>
2026-03-25 22:02:16 +0100

    Program test

--- APBD_cw3/Program.cs
+++ APBD_cw3/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.Write("test");
+﻿using APBD_cw3;
+
+Console.Write("test");
+
+Employee emp1 = new Employee("Szymon", "Szot");
+Student st1 = new Student("Szmon", "Szmon");
+
+Camera cam = new Camera("nikon", "4k", "sss");
+Projector proj = new Projector("hitachi", 6000, "HDMI");
+Laptop lap = new Laptop("Dell", "i5", 8);
+
+/*Service service = new Service();
+service.Items.Add(cam);
+service.Items.Add(lap);
+service.Items.Add(proj);
+
+service.Users.Add(emp1);
+service.Users.Add(st1);
+
+service.ShowAllEquipment();
+service.AddRenting(1, 1);
+
+service.ShowEquipmentByUser(1);*/
